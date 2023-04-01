c9b6fe9ba44e7c8d127eaae1ca01690701a65cb3
anggalabs <112769418+anggalabs@users.noreply.github.com>
2023-04-01 16:55:57 +0700

    Create quotes_spider.py

---
+++ News Analytics/emiten/emiten/spiders/quotes_spider.py
@@ -0,0 +1,18 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "TLKM"
+
+    def start_requests(self):
+        urls = [
+            'http://www.cnbcindonesia.com',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        #filename = f'quotes-{page}.html'
+        #with open(filename, 'wb') as f:
+        #    f.write(response.body)
+        #self.log(f'Saved file {filename}')
