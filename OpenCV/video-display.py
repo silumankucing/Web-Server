90962f563db949c5b00e7061282e09a5e8c70871
anggalabs <112769418+anggalabs@users.noreply.github.com>
2023-04-01 16:42:29 +0700

    Create video-display.py

---
+++ OpenCV/video-display.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture('src/submarine.mp4')
+
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
+
+while(cap.isOpened()):
+  ret, frame = cap.read()
+  if ret == True:
+    cv2.imshow('Video Display',frame)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
+  else: 
+    break
+
+cap.release()
+cv2.destroyAllWindows()
