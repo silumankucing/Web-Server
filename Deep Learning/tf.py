f934cdf1929ffba61c7472d6b0fb2c3488e60438
kuro <112769418+kucingkuro@users.noreply.github.com>
2024-01-31 21:08:12 +0700

    s

--- Deep Learning/tf.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-x1 = tf.constant(5)
-x2 = tf.constant(6)
-
-printf(x1)
--- OCR/test.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import pytesseract
-from pytesseract import Output
-
-img = cv2.imread('invoice.jpg')
-
-d = pytesseract.image_to_data(img, output_type=Output.DICT)
-print(d.keys())
